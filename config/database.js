d7ceec965a5faab19ebde45c0a78ce83a50c1c0a
Jordi Sabaté <jordi@webcoop.cat>
2020-11-27 15:44:46 +0100

    mysql added

--- config/database.js
+++ config/database.js
@@ -3,11 +3,17 @@ module.exports = ({ env }) => ({
   connections: {
     default: {
       connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/resilience.db'),
+      settings: {        
+        client: env('DATABASE_CLIENT', 'sqlite'),
+        filename: env('DATABASE_FILENAME', '.tmp/resilience.db'),        
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        username: env('DATABASE_USERNAME', ''),
+        password: env('DATABASE_PASSWORD', ''),
       },
       options: {
+        ssl: false,
         useNullAsDefault: true,
       },
     },
