2fb772f42d7cbbcb5f49a76f6b9a306be85f6b6d
Jordi Sabaté <jordi@webcoop.cat>
2021-03-09 14:40:01 +0100

    server URL

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('URL', 'https://api.example.com'),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '34dd651ee4ff9a6cb2aae6393f6d90f7'),
